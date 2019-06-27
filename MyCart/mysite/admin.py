01da3e27bf41d84d00134961766ec6703e002783
JakeTyler <jiteshkbudhwani@gmail.com>
2019-06-27 08:14:36 +0530

    Small changes to admin.py

--- MyCart/mysite/admin.py
+++ MyCart/mysite/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Product
+
+admin.site.register(Product, ProductAdmin)
